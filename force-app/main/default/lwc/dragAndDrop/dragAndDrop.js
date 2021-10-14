186df5174b3129fd0a58559b72b962e121689b94
Tikhon <tykhon.lavrov@koroka.solutions>
2021-10-14 13:04:21 +0300

    Raw Drag and Drop Implementation Without Styling or Data From Apex

--- force-app/main/default/lwc/dragAndDrop/dragAndDrop.js
+++ force-app/main/default/lwc/dragAndDrop/dragAndDrop.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class DragAndDrop extends LightningElement {} 
+export default class DragAndDrop extends LightningElement {
+    dragStart(event){
+        event.dataTransfer.setData("elementId", event.target.id);
+    }
+
+    allowDrop(event){
+        event.preventDefault();
+    }
+    
+    drop(event){
+        event.preventDefault();
+        let elementId = event.dataTransfer.getData("elementId");
+        let draggedElement = this.template.querySelector('#' + elementId);
+        event.target.appendChild(draggedElement);
+    }
+} 
+
