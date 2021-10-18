47f686d230ff630a730c6ac7aa0af22a89d48d6f
Tikhon <tykhon.lavrov@koroka.solutions>
2021-10-18 12:48:23 +0300

    Added Data From Apex Controller

--- force-app/main/default/lwc/dragAndDrop/dragAndDrop.js
+++ force-app/main/default/lwc/dragAndDrop/dragAndDrop.js
@@ -1,8 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import getAccounts from '@salesforce/apex/dragAndDropController.getAccounts';
 
 export default class DragAndDrop extends LightningElement {
+
+    @track accounts;
+    @track error;
+
+    @wire(getAccounts)
+    wiredAccounts({data,error}){
+        if(data){
+            this.accounts = data;
+            this.error = undefined;
+        }else{
+            this.accounts = undefined;
+            this.error = error;
+        }
+    }
+
     dragStart(event){
         event.dataTransfer.setData("elementId", event.target.id);
+        console.log(event.dataTransfer.getData("elementId"));
+        console.log(event.target.id);
     }
 
     allowDrop(event){
@@ -12,7 +30,10 @@ export default class DragAndDrop extends LightningElement {
     dropFirst(event){
         event.preventDefault();
         let elementId = event.dataTransfer.getData("elementId");
+        console.log(elementId);
+        
         let draggedElement = this.template.querySelector('#' + elementId);
+        console.log(draggedElement);
         draggedElement.classList.remove('draggable-style-second-container');
         draggedElement.classList.add('draggable-style-first-container'); 
         event.target.appendChild(draggedElement);
@@ -21,7 +42,9 @@ export default class DragAndDrop extends LightningElement {
     dropSecond(event){
         event.preventDefault();
         let elementId = event.dataTransfer.getData("elementId");
+        console.log(elementId);
         let draggedElement = this.template.querySelector('#' + elementId);
+        console.log(draggedElement);
         draggedElement.classList.remove('draggable-style-first-container');
         draggedElement.classList.add('draggable-style-second-container'); 
         event.target.appendChild(draggedElement);
